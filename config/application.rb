50aa38c7d6eb525ef28160646c105230d15933cc
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-07-29 23:01:15 -0700

    create new demo controller

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,8 @@ module RainVue
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators.helper = false
+    config.generators.stylesheets = false
   end
 end
